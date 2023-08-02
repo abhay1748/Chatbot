12f5d17b692f64e05ef38f37c45b3ca357b4e6d0
Abhay Arya <124766673+abhay1748@users.noreply.github.com>
2023-08-02 19:27:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const form = document.getElementById("chat-form");
+const input = document.getElementById("chat-input");
+const messages = document.getElementById("chat-messages");
+const apiKey = "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";
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const message = input.value;
+  input.value = "";
+
+  messages.innerHTML += `<div class="message user-message">
+  <span>${message}</span>
+  </div>`;
+
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        prompt: message,
+        model: "text-davinci-003",
+        temperature: 0,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+    
+    const chatbotResponse = response.data.choices[0].text;
+
+    messages.innerHTML += `<div class="message bot-message">
+     <span>${chatbotResponse}</span>
+    </div>`;
+  } catch (error) {
+    console.error("Error in API call:", error.response ? error.response.data : error);
+    messages.innerHTML += `<div class="message bot-message">
+     <span>Oops, something went wrong with the API call. Please try again later.</span>
+    </div>`;
+  }
+  
+  
+});
+
+
